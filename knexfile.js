0f6db97c22ec74ea43380b7968322d1395564ac9
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2016-12-14 17:18:51 -0700

    deploying

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,14 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/beerTasting'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
-  // }
-  //
-  // production: {
-  //   client: 'pg',
-  //   connection: {
-  //     database:
-  //
-  // }
-
 };
